38853975a7bafd9e12fe96123e3bc83ce97bfbd6
Mark Harviston <harvimt@pdx.edu>
2011-08-13 14:33:36 -0700

    updated edit_user and added del_confirm user, might work maybe

--- flaskapp/user.py
+++ flaskapp/user.py
@@ -32,11 +32,17 @@ class UserForm(Form):
 def user_name_does_not_exist(el, state):
 	return sess.query(User).get(el['user_name'].value) is None
 
+def user_name_exists(el, state):
+	return sess.query(User).get(el['user_name'].value) is None
+
 def odin_name_exists(el,state):
 	return get_odin(el['user_name'].value) is not None
 
 class NewUserForm(UserForm):
 	validators = [user_name_does_not_exist, odin_name_exists]
+
+class EditUserForm(UserForm):
+	validators = [user_name_exists, odin_name_exists]
 
 ## Odin/LDAP helpers ##
 
@@ -71,49 +77,28 @@ def list_users():
 	users = sess.query(User).all()
 	return render_response('list_users.html',dict(users=users))
 
-@app.route('/user/new')
+@app.route('/user/new',methods=['POST','GET'])
 def new_user():
-	"""Display new user creation form"""
-	require_auth('admin')
-
-	if flask.g is None:
-		form = NewUserForm()
-	else:
-		form = flask.g
-		app.logger.debug('Old Form: %r' % form)
-		flask.g = None
-
-	return render_response('add_user.html', dict(form=form,submit_url=url_for('new_user_submit')))
-
-@app.route('/user/new/submit',methods=['POST'])
-def new_user_submit():
-	"""Process new user creation form"""
+	"""Display new user creation form or accept and process it"""
 	require_auth('admin')
 
 	form = NewUserForm()
 
-	#app.logger.debug('New User Submitted with type: %s' % type(request.form))
+	if request.method == 'POST':
+		values = request.form.copy()
+		del values['_csrf_token']
+		form.set(values)
 
-	values = request.form.copy()
-	del values['_csrf_token']
-	
-	app.logger.debug('New User Submitted with %r' % values)
+		if form.validate():
+			user = User(**(form.value))
+			sess.add(user)
+			sess.commit()
+			flash('Successfully added new user')
+			return redirect(url_for('display_user',username=form['user_name'].value))
+		else:
+			flash('Form Validation Failed','error')
 
-	form.set(values)
-
-	app.logger.debug('New User Form Has %r' % form.value)
-
-	if form.validate():
-		user = User(**form)
-		#sess.add(user)
-		#sess.commit()
-		flash('Successfully added new user')
-		return redirect(url_for('display_user',username=form['user_name'].value))
-	else:
-		#TODO
-		flash('Form Validation Failed')
-		flash.g = form
-		return redirect(url_for('new_user'))
+	return render_response('add_user.html', dict(form=form,submit_url=url_for('new_user')))
 
 @app.route('/user/<username>')
 def display_user(username):
@@ -127,7 +112,7 @@ def display_user(username):
 	oinfo = get_odin(username)
 	if oinfo is None:
 		flash('User info not found in LDAP','error')
-		user.email = 'unkown'
+		user.email = 'unknown'
 		user.full_name = 'unknown'
 	else:
 		user.email = oinfo['email']
@@ -135,14 +120,53 @@ def display_user(username):
 
 	return render_response('display_user.html', dict(user=user))
 
-@app.route('/user/<username>/edit')
+@app.route('/user/<username>/edit',methods=['GET','POST'])
 def edit_user(username):
 	"""Edit the user's info"""
+	require_auth('admin')
 
 	user = sess.query(User).get(username)
-	return 'Edit info for ' + username
+	if user is None: abort(404)
 
-@app.route('/user/<username>/edit/submit',methods=['POST'])
-def edit_user_submit(username):
-	"""Process Edit user submit"""
-	return 'Process Edit Form ' + username
+	form = EditUserForm.from_object(user)
+
+	if request.method == 'POST':
+		values = request.form.copy()
+		del values['_csrf_token']
+
+		if 'delete' in values:
+			return render_response('del_confirm_user.html',dict(submit_url=url_form('del_confirm_user',username=username)))
+
+		form.set(values)
+
+		form.validate()
+
+		if form['user_name'].value != username:
+			form.add_error('username in url does not match that in form')
+
+		if not form.valid:
+			flash('Form Invalid','error')
+		else:
+			user.__dict__.update(form.value)
+			sess.commit()
+			flash('Successfully Updated User Info')
+
+	user = sess.query(User).get(username)
+	return render_response('edit_user.html',dict(form=form,submit_url=url_for('edit_user')))
+
+@app.route('/user/<username>/confirm_delete', methods=['POST'])
+def del_confirm_user(username):
+	require_auth('admin')
+
+	user = sess.query(User).get(username)
+	if user is None: abort(404)
+
+	if request.form['confirm'] == 'yes':
+		sess.delete(user)
+		sess.commit()
+
+		flash('Successfully deleted %s' % username)
+		return redirect(url_for('list_users'))
+	else:
+		return redirect(url_for('edit_user',username=username))
+
