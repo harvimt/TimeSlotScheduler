5d7f25aa6d0ef002c3ddd047ce5ab71ee4f6c18d
Thom Linton <thom.linton@gmail.com>
2011-08-31 18:11:58 -0700

    Adding driver to allow the creation of a system-level user account from the command line (for bootstrapping purposes)

---
+++ createuser.py
@@ -0,0 +1,17 @@
+#!./env/bin/python
+
+from flaskapp.datamodel import User
+from flaskapp.database import db_session as sess
+
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "Usage: createuser.py [username]"
+        sys.exit(1)
+
+    username = sys.argv[1]
+    user = User(**{'user_name':username,'user_type':'admin'})
+    sess.add(user)
+    sess.commit()
