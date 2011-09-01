950cd5d25a0755aab689249c5db453e1eba99c9d
Thom Linton <thom.linton@gmail.com>
2011-08-31 18:21:58 -0700

    Adding flexibility to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-if test -f /usr/bin/virtualenv; then
+# XXX: Change the following to suit 
+VIRTUALENV_EXECUTABLE='/usr/bin/virtualenv';
+
+echo "Using VIRTUALENV_EXECUTABLE=${VIRTUALENV_EXECUTABLE}"
+
+if test -f ${VIRTUALENV_EXECUTABLE}; then
   if ! test -x ./env; then
-    virtualenv --no-site-packages env
+    ${VIRTUALENV_EXECUTABLE} --no-site-packages env
   fi
   if test -f ./env/bin/python && test -f ./env/bin/pip; then
     env/bin/pip install -r requirements.txt
@@ -16,5 +21,8 @@ if test -f /usr/bin/virtualenv; then
   fi
 else
   echo "You must install python-virtualenv in order to build this project."
+  echo "Check to see that it isn't already installed under a non-standard name, e.g., virtualenv-2.6."
+  echo "If so, modify 'VIRTUALENV_EXECUTABLE' at the top of the bootstrap script"
+  echo
   echo "Please see http://pypi.python.org/pypi/virtualenv for more details"
 fi
