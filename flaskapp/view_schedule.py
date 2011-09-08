203721855e28b7b72862c5518133487c52f2aa2c
Mark Harviston <harvimt@pdx.edu>
2011-09-07 18:13:15 -0700

    view_schedule as csv works, StringIO implementaiton seems takcy

--- flaskapp/view_schedule.py
+++ flaskapp/view_schedule.py
@@ -14,7 +14,7 @@ View The Schedule
 import csv, operator, datetime
 
 import flask
-from flask import session, request, url_for, redirect, abort, flash
+from flask import session, request, url_for, redirect, abort, flash, make_response, send_file
 
 from flaskext.genshi import render_response
 from flaskapp.globals import *
@@ -23,6 +23,7 @@ from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import NoResultFound
 from database import db_session as sess
 from datamodel import Schedule
+from StringIO import StringIO
 
 @app.route('/view_schedule')
 def view_schedule():
@@ -37,8 +38,33 @@ def view_schedule():
 
 @app.route('/schedule.csv')
 def view_schedule_as_csv():
-	pass
+	#TODO not sure if this is the best way to do it, seems like the output buffer could be opened directly, or something
+	w_file = StringIO()
+	gen_csv(w_file)
+	w_file.seek(0) #rewind to beginning
+	return send_file(w_file, 'text/csv', as_attachment=False, attachment_filename='schedule.csv')
+	#return send_file(w_file, 'text/plain')
 
 def gen_csv(w_file):
-	pass
+	schedule = sess.query(Schedule).one()
+	assignments = schedule.assignments
 
+	#Calculate statistics
+	total_cost = sum(map(lambda x: x.cost, assignments))
+	avg_cost = total_cost/len(assignments)
+
+	writer = csv.writer(w_file)
+	writer.writerow(('CRN','Time','Theme','Faculty','Mentor'))
+	for assn in assignments:
+		prefs = assn.course.prefs_as_dict()
+		writer.writerow((
+			assn.course.crn,
+			prefs['Time'],
+			prefs['Theme'],
+			prefs['Faculty'],
+			assn.mentor.full_name
+		))
+
+	writer.writerow([])
+	writer.writerow(('Total Cost', total_cost))
+	writer.writerow(('Average Cost', avg_cost))
